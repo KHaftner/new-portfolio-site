24efb658eba0a8379a9f773f2e3f701936e10c47
KHaftner <Kassandra.Haftner@outlook.com>
2023-11-23 22:53:37 -0800

    Added pictures and started basic styling.

--- new-portfolio-site/src/App.jsx
+++ new-portfolio-site/src/App.jsx
@@ -16,7 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Kassandra Haftner</h1>
+      <h2>Web Developer.</h2>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -31,5 +32,6 @@ function App() {
     </>
   )
 }
+
 
 export default App
